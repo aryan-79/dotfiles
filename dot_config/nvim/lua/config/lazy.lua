a9977f5ecf725c5a0183278d852a0d7ac3346c1d
aryan <aryanmgr79@gmail.com>
2025-11-06 21:00:23 +0545

    disable which key

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -47,7 +47,6 @@ require("lazy").setup({
         "tohtml",
         "tutor",
         "zipPlugin",
-        "noice",
       },
     },
   },
--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -26,4 +26,8 @@ return {
     "rcarriga/nvim-notify",
     enabled = false,
   },
+  {
+    "folke/which-key.nvim",
+    enabled = false,
+  },
 }
